9a24cceb989bbd6581dcda485cee5e0952f9a9ba
Jacob Vibert <jvpokeyjake@gmail.com>
2019-08-12 12:54:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,8 +4,13 @@
 # or whitespace (' ').
 class EmailAddressParser
 attr_accessor :emails
+
 def initialize(emails)
     @emails = emails
   end
 
+def parse
+    emails.split(/, | /).uniq
+  end
+end
 end
